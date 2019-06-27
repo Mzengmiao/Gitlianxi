5d9e7756cc3197cb7612d15a83f4a1bc21b8113b
Mrzengmiao <you@example.com>
2019-06-27 15:15:58 +0800

    新项目

---
+++ app/src/main/java/com/gitlianxi/MainActivity.java
@@ -0,0 +1,26 @@
+package com.gitlianxi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_tv)
+    Button btnTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_tv)
+    public void onViewClicked() {
+    }
+}
